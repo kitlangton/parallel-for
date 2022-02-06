0fc6bd93c8462fbadee28cefa5423e1806e510d6
Kit Langton <kit.langton@gmail.com>
2022-02-06 11:20:11 -0500

    fix free parallel

--- src/test/scala/parallelfor/test/FreeParallel.scala
+++ src/test/scala/parallelfor/test/FreeParallel.scala
@@ -13,12 +13,15 @@ sealed trait FreeParallel[-R, +E, +A] extends Product with Serializable { self =
 
   def traceImpl: (Trace, A)
 
-  def flatMap[R1 <: R, E1 >: E, B](f: A => FreeParallel[R1, E1, B])(implicit
-      dummyImplicit: DummyImplicit
-  ): FreeParallel[R1, E1, B] =
+  def zipPar[R1 <: R, E1 >: E, B](that: FreeParallel[R1, E1, B])(implicit
+      zippable: Zippable[A, B]
+  ): FreeParallel[R1, E1, zippable.Out] =
+    FreeParallel.ZipPar(self, that).map { case (a, b) => zippable.zip(a, b) }
+
+  def flatMap[R1 <: R, E1 >: E, B](f: A => FreeParallel[R1, E1, B]): FreeParallel[R1, E1, B] =
     FreeParallel.FlatMap(this, f)
 
-  def map[R1 <: R, E1 >: E, B](f: A => B)(implicit dummyImplicit: DummyImplicit): FreeParallel[R1, E1, B] =
+  def map[B](f: A => B): FreeParallel[R, E, B] =
     FreeParallel.Map(this, f)
 }
 
@@ -26,7 +29,7 @@ object FreeParallel {
   def effect[A](a: A): FreeParallel[Any, Nothing, A] =
     FreeParallel.Succeed(a)
 
-  final case class ZipPar[R, E, A, B](left: FreeParallel[R, E, A], right: FreeParallel[R, E, B])
+  final case class ZipPar[-R, +E, +A, +B](left: FreeParallel[R, E, A], right: FreeParallel[R, E, B])
       extends FreeParallel[R, E, (A, B)] {
     override def traceImpl: (Trace, (A, B)) = {
       val (lhsTrace, lhsValue) = left.traceImpl
@@ -36,7 +39,7 @@ object FreeParallel {
     }
   }
 
-  final case class FlatMap[R, E, A, B](fa: FreeParallel[R, E, A], f: A => FreeParallel[R, E, B])
+  final case class FlatMap[-R, +E, A, +B](fa: FreeParallel[R, E, A], f: A => FreeParallel[R, E, B])
       extends FreeParallel[R, E, B] {
 
     override def traceImpl: (Trace, B) = {
@@ -47,7 +50,7 @@ object FreeParallel {
     }
   }
 
-  final case class Map[R, E, A, B](fa: FreeParallel[R, E, A], f: A => B) extends FreeParallel[R, E, B] {
+  final case class Map[-R, +E, A, +B](fa: FreeParallel[R, E, A], f: A => B) extends FreeParallel[R, E, B] {
 
     override def traceImpl: (Trace, B) = {
       val (faTrace, faValue) = fa.traceImpl
